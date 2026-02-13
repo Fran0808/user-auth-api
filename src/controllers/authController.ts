c48f7184c41d4e401fe4d091926cefd7f5d6eceb
Fran0808 <fortegaquipse@gmail.com>
2026-02-13 15:39:27 -0500

    feat: create athRoutes

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,23 @@
 import type { Request, Response } from "express";
 
 export const register = (req: Request, res: Response) => {
-    console.log(req.body);
-    res.json({ message: "Register endpoint working" });
+
+    if (!req.body) {
+        return res.status(400).json({
+            message: "No data sent"
+        });
+    }
+
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({
+            message: "All fields are required"
+        })
+    }
+
+    res.json({
+        message: "Register endpoint working",
+        data: { name, email }
+    });
 };
