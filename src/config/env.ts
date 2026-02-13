e5ed1795ed9a60e27d1827f402272035fb48e99c
Fran0808 <fortegaquipse@gmail.com>
2026-02-13 11:21:59 -0500

    feat: add env.ts  to capture .env values

---
+++ src/config/env.ts
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+
+//Load file contents into process.env by default
+dotenv.config();
+
+function getEnvVariable(key: string): string {
+    const value = process.env[key];
+
+    if (!value) {
+        throw new Error(`Missing required environment variable: ${key}`);
+    }
+
+    return value;
+}
+
+export const config = {
+    port: process.env.DB_PORT || 3000,
+
+    db: {
+        host: getEnvVariable("DB_HOST"),
+        port: Number(getEnvVariable("DB_PORT")),
+        user: getEnvVariable("DB_USER"),
+        password: getEnvVariable("DB_PASSWORD"),
+        name: getEnvVariable("DB_NAME"),
+    },
+
+    jwt: {
+        secret: getEnvVariable("JWT_SECRET"),
+        expiresIn: getEnvVariable("JWT_EXPIRES_IN"),
+    },
+
+    externalApi: {
+        baseUrl: getEnvVariable("EXTERNAL_API_URL"),
+    },
+
+}
