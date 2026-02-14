f4ec1246cf0d40910465fc7d6498638c0ea48903
Fran0808 <fortegaquipse@gmail.com>
2026-02-14 11:14:42 -0500

    feat: add correct route to login

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,5 +4,6 @@ import { login, register } from "../controllers/authController.js";
 const router = Router();
 
 router.post("/register", register);
-router.post("login", login)
+router.post("/login", login)
+
 export default router;
