dc64de1c958e286e8b5e19176d29a622d237acee
Fran0808 <fortegaquipse@gmail.com>
2026-02-14 10:40:06 -0500

    feat: create loginUser

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import { findByEmail, createUser } from "../repositories/userRepository.js";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (name: string, email: string, password: string) => {
     const existingUser = await findByEmail(email);
@@ -16,4 +17,28 @@ export const registerUser = async (name: string, email: string, password: string
     const { password: _, ...userWithoutPassword } = newUser;
 
     return userWithoutPassword;
+}
+
+export const loginUser = async (email: string, password: string) => {
+    const user = await findByEmail(email);
+
+    if (!user) {
+        throw new Error("Invalid credentials");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        throw new Error("Invalid credentials");
+    }
+
+    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET as string, { expiresIn: "1h" });
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return {
+        message: "Login succesfully",
+        token,
+        user: userWithoutPassword
+    };
 }
