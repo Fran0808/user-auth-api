4e52d3735a0579a73ca34bc6ae869b2173a42518
Fran0808 <fortegaquipse@gmail.com>
2026-02-14 11:47:16 -0500

    feat: protecting routes with middleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,33 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({
+            message: "Access denied. No token provided"
+        });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Access denied. Malformed token"
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as jwt.JwtPayload;
+
+        (req as any).user = decoded;
+
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            message: "Invalid or expired token"
+        });
+    }
+
+}
