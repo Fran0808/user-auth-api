e95039ff33484a6c4794284299bff0c1b33597be
Fran0808 <fortegaquipse@gmail.com>
2026-02-16 16:02:00 -0500

    feat: add loginSchema

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -5,3 +5,7 @@ export const registerSchema = z.object({
     email: z.email("Invalid email format"),
     password: z.string().min(6, "The password must have at least 6 characters"),
 });
+export const loginSchema = z.object({
+    email: z.email("Invalid email format"),
+    password: z.string().min(1, "Password is required"),
+});
