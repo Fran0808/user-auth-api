df4c73c96693c9edb40b816ca88813f3dd7594de
Fran0808 <fortegaquipse@gmail.com>
2026-02-16 16:23:42 -0500

    feat: use the middleware rate limit in authRoutes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { login, register, getProfile } from "../controllers/authController.js";
 import { authenticateToken } from "../middlewares/authMiddleware.js";
+import { authLimiter } from "../middlewares/rateLimiter.js";
 
 const router = Router();
+
+router.use(authLimiter);
 
 router.post("/register", register);
 router.post("/login", login)
