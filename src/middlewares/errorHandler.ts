9d3d04bae2bfd75f4705b5036b684e166faf60b0
Fran0808 <fortegaquipse@gmail.com>
2026-02-17 18:20:50 -0500

    feat: implement centralized error handler middleware

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,18 @@
+import type { Request, Response, NextFunction } from "express";
+
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error("### ERROR LOG ###");
+    console.error(err);
+    console.error("#################");
+
+    const status = err.status || 500;
+    const message = err.message || "Internal Server Error";
+
+    res.status(status).json({
+        success: false,
+        status,
+        message,
+
+        ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
+    });
+};
